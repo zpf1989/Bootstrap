5feeb51dd0029c82bdc76e9f2cffb3a9ede6415b
zpf1989 <751682472@qq.com>
2016-05-24 16:22:27 +0800

    修改

--- cityselect/city.js
+++ cityselect/city.js
@@ -1,50 +1,63 @@
-var cities = [
-    { id: 'jinan', text: '济南' },
-    { id: 'taian', text: '泰安' },
-    { id: 'weifang', text: '潍坊' },
-    { id: 'dezou', text: '德州' },
-    { id: 'binzhou', text: '滨州' },
-    { id: 'laiwu', text: '莱芜' },
-    { id: 'qingdao', text: '青岛' },
-    { id: 'yantai', text: '烟台' },
-    { id: 'rizhao', text: '日照' },
-    { id: 'dongying', text: '东营' },
-    { id: 'jining', text: '济宁' },
-    { id: 'heze', text: '菏泽' },
-    { id: 'liaocheng', text: '聊城' },
-    { id: 'linyi', text: '临沂' },
-    { id: 'zaozhuang', text: '枣庄' },
-    { id: 'zibo', text: '淄博' },
-    { id: 'weihai', text: '威海' }
-];
-var counties = [
-    { cityid: "jinan", id: "lxq", text: "历下区" },
-    { cityid: "jinan", id: "szq", text: "市中区" },
-    { cityid: "jinan", id: "hyq", text: "槐荫区" },
-    { cityid: "jinan", id: "tqq", text: "天桥区" },
-    { cityid: "jinan", id: "lcq", text: "历城区" },
-    { cityid: "jinan", id: "cqq", text: "长清区" },
-    { cityid: "jinan", id: "gxq", text: "高新区" },
-    { cityid: "jinan", id: "pyx", text: "平阴县" },
-    { cityid: "jinan", id: "jyx", text: "济阳县" },
-    { cityid: "jinan", id: "shx", text: "商河县" },
-    { cityid: "jinan", id: "zqs", text: "章丘市" }
-];
+var cityselector = {
+    cities: [
+        { id: 'jinan', text: '济南' },
+        { id: 'taian', text: '泰安' },
+        { id: 'weifang', text: '潍坊' },
+        { id: 'dezou', text: '德州' },
+        { id: 'binzhou', text: '滨州' },
+        { id: 'laiwu', text: '莱芜' },
+        { id: 'qingdao', text: '青岛' },
+        { id: 'yantai', text: '烟台' },
+        { id: 'rizhao', text: '日照' },
+        { id: 'dongying', text: '东营' },
+        { id: 'jining', text: '济宁' },
+        { id: 'heze', text: '菏泽' },
+        { id: 'liaocheng', text: '聊城' },
+        { id: 'linyi', text: '临沂' },
+        { id: 'zaozhuang', text: '枣庄' },
+        { id: 'zibo', text: '淄博' },
+        { id: 'weihai', text: '威海' }
+    ],
+    counties: [
+        { cityid: "jinan", id: "lxq", text: "历下区" },
+        { cityid: "jinan", id: "szq", text: "市中区" },
+        { cityid: "jinan", id: "hyq", text: "槐荫区" },
+        { cityid: "jinan", id: "tqq", text: "天桥区" },
+        { cityid: "jinan", id: "lcq", text: "历城区" },
+        { cityid: "jinan", id: "cqq", text: "长清区" },
+        { cityid: "jinan", id: "gxq", text: "高新区" },
+        { cityid: "jinan", id: "pyx", text: "平阴县" },
+        { cityid: "jinan", id: "jyx", text: "济阳县" },
+        { cityid: "jinan", id: "shx", text: "商河县" },
+        { cityid: "jinan", id: "zqs", text: "章丘市" }
+    ],
 
-var districts = [
-    { countyid: 'lxq', id: 'dljd', text: '甸柳街道' },
-    { countyid: 'lxq', id: 'shjd', text: '舜华街道' },
-    { countyid: 'lxq', id: 'qcljd', text: '泉城路街道' },
-    { countyid: 'lxq', id: 'dmhjd', text: '大明湖街道' },
-    { countyid: 'lxq', id: 'dgjd', text: '东关街道' },
-    { countyid: 'lxq', id: 'jzxcjd', text: '建筑新村街道' },
-    { countyid: 'lxq', id: 'qfsjd', text: '千佛山街道' },
-    { countyid: 'lxq', id: 'btqjd', text: '趵突泉街道' },
-    { countyid: 'lxq', id: 'jfljd', text: '解放路街道' },
-    { countyid: 'lxq', id: 'whdljd', text: '文化东路街道' },
-    { countyid: 'lxq', id: 'ysljd', text: '燕山路街道' },
-    { countyid: 'lxq', id: 'dlxcjd', text: '甸柳新村街道' },
-    { countyid: 'lxq', id: 'yj', text: '姚家' },
-    { countyid: 'lxq', id: 'zy', text: '智远' },
-    { countyid: 'lxq', id: 'ldjd', text: '龙洞街道' }
-];
+    districts: [
+        { countyid: 'lxq', url: 'http://www.dljd.com:8080', id: 'dljd', text: '甸柳街道' },
+        { countyid: 'lxq', url: '#', id: 'shjd', text: '舜华街道' },
+        { countyid: 'lxq', url: '#', id: 'qcljd', text: '泉城路街道' },
+        { countyid: 'lxq', url: '#', id: 'dmhjd', text: '大明湖街道' },
+        { countyid: 'lxq', url: '#', id: 'dgjd', text: '东关街道' },
+        { countyid: 'lxq', url: '#', id: 'jzxcjd', text: '建筑新村街道' },
+        { countyid: 'lxq', url: '#', id: 'qfsjd', text: '千佛山街道' },
+        { countyid: 'lxq', url: '#', id: 'btqjd', text: '趵突泉街道' },
+        { countyid: 'lxq', url: '#', id: 'jfljd', text: '解放路街道' },
+        { countyid: 'lxq', url: '#', id: 'whdljd', text: '文化东路街道' },
+        { countyid: 'lxq', url: '#', id: 'ysljd', text: '燕山路街道' },
+        { countyid: 'lxq', url: '#', id: 'dlxcjd', text: '甸柳新村街道' },
+        { countyid: 'lxq', url: '#', id: 'yj', text: '姚家' },
+        { countyid: 'lxq', url: '#', id: 'zy', text: '智远' },
+        { countyid: 'lxq', url: '#', id: 'ldjd', text: '龙洞街道' }
+    ],
+
+    communities: [
+        { districtid: 'dljd', url: 'http://www.dlsq1.com:8080', id: 'dlsq1', text: '甸柳新村第一社区' },
+        { districtid: 'dljd', url: '#', id: 'dlsq2', text: '甸柳新村第二社区' },
+        { districtid: 'dljd', url: '#', id: 'dlsq3', text: '甸柳新村第三社区' },
+        { districtid: 'dljd', url: '#', id: 'dlsq4', text: '甸柳新村第四社区' },
+        { districtid: 'dljd', url: '#', id: 'dlsq5', text: '甸柳新村第五社区' },
+        { districtid: 'dljd', url: '#', id: 'dbsq', text: '甸北社区' },
+        { districtid: 'dljd', url: '#', id: 'dnsq', text: '甸南社区' },
+        { districtid: 'dljd', url: '#', id: 'jxysq', text: '吉祥苑社区' }
+    ]
+};
